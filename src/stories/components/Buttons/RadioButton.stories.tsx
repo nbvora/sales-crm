03fe2ac55707b5e60a854f008855cff7fdda8887
harshnstack <harsh.nstack@gmail.com>
2022-10-30 12:39:31 +0530

    create a RadioButton and SwitchButton component

---
+++ src/stories/components/Buttons/RadioButton.stories.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import { RadioButton } from "./RadioButton";
+
+export default {
+  title: "RadioButton",
+  component: RadioButton,
+  argTypes: {
+    backgroundColor: { control: "color" },
+  },
+} as unknown as ComponentMeta<typeof RadioButton>;
+
+const Template: ComponentStory<typeof RadioButton> = (args) => (
+  <RadioButton {...args} />
+);
+
+export const Primary = Template.bind({});
+Primary.args = {
+  label: "RadioButton",
+  label1: "RadioButton",
+};
---
+++ src/stories/components/Buttons/SwitchButton.stories.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import { SwitchButton } from "./SwitchButton";
+
+export default {
+  title: "SwitchButton",
+  component: SwitchButton,
+  argTypes: {
+    backgroundColor: { control: "color" },
+    hover: { control: "color" },
+  },
+} as unknown as ComponentMeta<typeof SwitchButton>;
+
+const Template: ComponentStory<typeof SwitchButton> = (args) => (
+  <SwitchButton {...args} />
+);
+
+export const Primary = Template.bind({});
+Primary.args = {
+  label: "Switch",
+};
