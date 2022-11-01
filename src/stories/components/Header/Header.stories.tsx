60781169bc97035d015ae25193d133b3b3df634a
jaynstack <jayn.nstack@gmail.com>
2022-11-01 14:56:08 +0530

    changes on folder

--- src/stories/components/Header/Header.stories.tsx
+++ src/stories/components/Header/Header.stories.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import NavBarr from "./Header";
 export default {
-  title: "Headercomponent",
+  title: "HeaderComponent",
   component: NavBarr,
 } as unknown as ComponentMeta<typeof NavBarr>;
 const Template: ComponentStory<typeof NavBarr> = () => <NavBarr />;
