7bebe0e9500992d48771ceff9cce743d584f981e
harshnstack <harsh.nstack@gmail.com>
2022-11-24 14:07:25 +0530

    update the rootSaga code for easy understanding

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,5 +1,6 @@
 const sagaActions = {
   INITIALIZE_SAGA: 'Initialize_Saga',
+  SIGNUP_SAGA: 'SIGNUP_SAGA',
 };
 
 export default sagaActions;
--- src/redux/sagas/loginSaga/registerSaga.js
+++ src/redux/sagas/loginSaga/registerSaga.js
@@ -1,12 +1,3 @@
-export function* signinSaga() {
-  try {
-    const data = yield { name: 'harsh' };
-    console.log(data);
-  } catch (e) {
-    // yield put({ type: "TODO_FETCH_FAILED" });
-  }
-}
-
 export function* signupSaga() {
   try {
     const data = yield { name: 'harsh', surname: 'limbachiya' };
