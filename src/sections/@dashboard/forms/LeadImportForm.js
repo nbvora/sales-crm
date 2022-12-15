7e5879b72b99ff322dbdab021059d17c5a520666
harshnstack <harsh.nstack@gmail.com>
2022-12-15 14:44:42 +0530

    remove the console warnings

--- src/sections/@dashboard/forms/LeadImportForm.js
+++ src/sections/@dashboard/forms/LeadImportForm.js
@@ -2,15 +2,12 @@ import { useState } from 'react';
 import FileUpload from 'react-material-file-upload';
 import { Box } from '@mui/system';
 import { Button } from '@mui/material';
-import { PATH_DASHBOARD } from '../../../routes/paths';
-import HeaderBreadcrumbs from '../../../components/HeaderBreadcrumbs';
 
 export default function LeadImportForm() {
   const [files, setFiles] = useState([]);
 
   return (
     <>
-      <HeaderBreadcrumbs heading="Import File" links={[{ href: PATH_DASHBOARD.lead.root }]} />
       <Box>
         <h1>Import File</h1>
         <FileUpload
