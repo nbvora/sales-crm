36b34531917a6a22e580febbf3dd888f4700209f
kunal rokhle <kunal.rokhle@nstacksoftech.com>
2022-12-23 16:42:45 +0530

    dashboard API

--- src/redux/sagas/dashboardSaga/stockSaga.js
+++ src/redux/sagas/dashboardSaga/stockSaga.js
@@ -1,7 +1,32 @@
 import { put } from 'redux-saga/effects';
-import { getStokes } from '../../slices/dashboard';
+import { getStokes, startLoading } from '../../slices/dashboard';
+import axios from '../../../utils/axios';
 import { _userList } from '../../../_mock';
+import { BASEURL } from '../../../BaseUrl/BaseUrl';
+import { dispatch } from '../../store';
+import sagaActions from '../../actions';
+// import { dispatch, useSelector } from '../../../redux/store';
+// import sagaActions from '../../../redux/actions';
+
+// export function* stockSaga() {
+//   yield put(getStokes(_userList));
+// }
 
 export function* stockSaga() {
-  yield put(getStokes(_userList));
+  try {
+    yield put(getStokes(_userList));
+    yield put(startLoading());
+    const Token = window.localStorage.getItem('token');
+    const response = yield axios.get(`${BASEURL}dashboard`, {
+      headers: {
+        authToken: `${Token}`,
+      },
+    });
+    const { userRoles } = response.data.data;
+    yield put(getStokes(userRoles));
+
+    // yield dispatch({ type: sagaActions.GET_STOCKES });
+  } catch (error) {
+    console.log(error);
+  }
 }
